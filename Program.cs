d1064e4434b4c42da41c6a87b32d6b82757969db
SilviaKristelSP <huachupango-@hotmail.com>
2023-06-19 23:12:38 -0600

    Cambios Accesibilidad

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using UVGramWeb;
+using UVGramWeb.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.Modal;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IHttpService, HttpService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddBlazoredModal();
+
+builder.Services.AddScoped(x =>
+{
+    var apiUrl = new Uri("http://192.168.10.148:8080");
+    return new HttpClient() { BaseAddress = apiUrl };
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.10.148:8080") });
+
+var host = builder.Build();
+var authService = host.Services.GetRequiredService<IAuthenticationService>();
+await authService.Initialize();
+
+await host.RunAsync();
